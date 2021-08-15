c9f414d3c037deece34da381f7defbc63cf218c1
hyunjungagnes <hyunjung.agnes.oh@gmail.com>
2021-08-15 08:19:41 +0800

    test: 💍 test for register

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from django.test.client import Client
+from user.models import MyUser
+from rest_framework.test import APIClient
 # Create your tests here.
+
+client = APIClient()
+
+
+class ResgisterTest(TestCase):
+    def test_register(self):
+        response = client.post(
+            '/api/user/register/', {'email': 'a@a.com', 'username': 'aaaa', 'password': 'aaaaaaaa'})
+        self.assertEqual(response.status_code, 201)
+
+    def test_username_alnumeric(self):
+        response = client.post(
+            '/api/user/register/', {'email': 'a@a.com', 'username': 'aaaa 123', 'password': 'aaaaaaaa'})
+        self.assertEqual(response.status_code, 400)
